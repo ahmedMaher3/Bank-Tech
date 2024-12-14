63c338f48b90cb92a6d075725fd08030c3e645e6
ahmedMaherE <asebaee@youxel.com>
2024-12-14 13:40:33 +0200

    Initial Commit

---
+++ Bank Tech/Bank_TechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bank_TechApp.swift
+//  Bank Tech
+//
+//  Created by ahmed maher on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Bank_TechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
