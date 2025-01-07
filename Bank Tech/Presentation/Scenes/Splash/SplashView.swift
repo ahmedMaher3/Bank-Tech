c792f810d1516b7639bb2b4a6454240c7621c199
ahmedMaherE <asebaee@youxel.com>
2025-01-07 17:13:42 +0200

    Bank Tech

---
+++ Bank Tech/Presentation/Scenes/Splash/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Bank Tech
+//
+//  Created by ahmed maher on 07/01/2025.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
