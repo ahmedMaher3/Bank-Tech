f6fca5b375157f0e0ea238a6b2f868e0b2e1af14
ahmedMaherE <asebaee@youxel.com>
2025-01-07 17:17:02 +0200

    Bank Tech

--- Bank Tech/Presentation/Scenes/Splash/SplashView.swift
+++ Bank Tech/Presentation/Scenes/Splash/SplashView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var isActive = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+           ZStack {
+               if isActive {
+                   MainView() // Navigate to the main content
+               } else {
+                   SplashContentView() // Show the splash screen
+                       .transition(.opacity) // Smooth fade-out transition
+                       .onAppear {
+                           // Delay for splash screen duration
+                           DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                               withAnimation {
+                                   isActive = true
+                               }
+                           }
+                       }
+               }
+           }
+       }
 }
 
+// MARK: - Splash Content
+struct SplashContentView: View {
+    var body: some View {
+        ZStack {
+            Color.white.edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+        }
+    }
+}
 #Preview {
     SplashView()
 }
